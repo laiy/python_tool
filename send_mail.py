64e4b298b733b8840f886273d80fbe885ee72b5d
laiy <ly.franky@gmail.com>
2014-11-13 11:21:45 +0800

    It's secret!

---
+++ send_mail.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+        Header(name, 'utf-8').encode(), \
+        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+from_addr = '834788686@qq.com'
+password = 'LAIYUAN941114'
+smtp_server = 'smtp.qq.com'
+to_addr = raw_input('Send to: ')
+title = raw_input("Title: ")
+content = raw_input('Content: ')
+
+msg = MIMEText(content, 'plain', 'utf-8')
+msg['From'] = _format_addr(u'LY<%s>' % from_addr)
+msg['To'] = _format_addr(u'<%s>' % to_addr)
+msg['Subject'] = Header(u'%s' % title, 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, 25)
+server.starttls()
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
+
