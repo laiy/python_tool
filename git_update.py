ac5b4ac80ecd7f7af4f9443207b3432ef2dd8bda
laiy <ly.franky@gmail.com>
2014-12-06 09:18:32 +0800

    Derp

---
+++ git_update.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#	> File Name: git_update.py
+#	> Author: LY
+#	> Mail: ly.franky@gmail.com
+#	> Created Time: Saturday, December 06, 2014 AM09:11:06 CST
+
+import sys
+import os
+
+def execCmd(cmd):
+    r = os.popen(cmd)
+    text = r.read()
+    r.close()
+    return text
+
+cmd = "git add -A"
+os.system(cmd)
+cmd = "git add ."
+os.system(cmd)
+cmd = '''git commit -m"`curl -s http://whatthecommit.com/index.txt`"'''
+os.system(cmd)
+cmd = "git push origin master"
+os.system(cmd)
+
